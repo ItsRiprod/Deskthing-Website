64b0d69043de0f3254cb771c0d6e48841d8fa6d8
ItsRiprod <riprodgames@gmail.com>
2025-03-30 07:08:14 -0700

    + Analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,35 @@
-import '../app/global.css';
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
+import "../app/global.css";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import React, { JSX, ReactNode } from 'react';
+import React, { JSX, ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: { title: string; description: string } = {
-  title: 'DeskThing',
-  description: 'Take back your CarThing.',
-}
+  title: "DeskThing",
+  description: "Take back your CarThing.",
+};
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
-    return (
-      <html className={`${GeistSans.variable} ${GeistMono.variable}`} lang="en">
-        <head>
-          <meta name="google-adsense-account" content="ca-pub-8018324056802093" />
-          <link rel="icon" href="imgs/AppIcon.png" />
-        </head>
-        <body className="bg-neutral-950 text-neutral-50">
-          <Navbar />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    )
+  return (
+    <html className={`${GeistSans.variable} ${GeistMono.variable}`} lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-8018324056802093" />
+        <link rel="icon" href="imgs/AppIcon.png" />
+      </head>
+      <body className="bg-neutral-950 text-neutral-50">
+        <Navbar />
+        {children}
+        <Footer />
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
 }
